367d226030f54e82ca1fedd3c0405ab312bbfb1c
Kevin-R-Ho <kevin.r.ho@outlook.com>
2021-09-21 23:28:02 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+
+function App() {
+  // importing switch allows multi page navigation via routing
+  return (
+    <Layout>
+      
+      <Switch>
+        <Route path='/' exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupsPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
+  )
+}
+
+export default App;
